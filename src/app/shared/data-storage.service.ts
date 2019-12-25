ecf1f05b266b705b5a65d6b2a1ecb86c28f4a91c
Felipe Apablaza <ing.apablaza@gmail.com>
2019-12-25 13:18:16 -0300

    Added store data

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,20 @@
+
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+  private recipeUrl = 'https://ng-course-recipe-book-80127.firebaseio.com/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  public storeRecipe(): void {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http.put(this.recipeUrl, recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+}
