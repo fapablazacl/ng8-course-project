38d118e529bf7932c65a41cef5ea7d02f6ee56fe
Felipe Apablaza <ing.apablaza@gmail.com>
2019-12-25 18:58:36 -0300

    Fixed indentation format

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -2,10 +2,10 @@
 import { Component, Input } from "@angular/core";
 
 @Component({
-    selector: 'app-alert',
-    templateUrl: './alert.component.html',
-    styleUrls: ['./alert.component.css']
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css']
 })
 export class AlertComponent {
-    @Input() message: string;
+  @Input() message: string;
 }
