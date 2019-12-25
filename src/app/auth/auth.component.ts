b8434d4023a8886246a36010ab5575fc630ce178
Felipe Apablaza <ing.apablaza@gmail.com>
2019-12-25 19:20:29 -0300

    Dynamic module cleanup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,7 @@
-import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService, AuthResponseData } from "./auth.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
 import { AlertComponent } from '../shared/alert/alert.component';
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
@@ -10,13 +10,21 @@ import { PlaceholderDirective } from "../shared/placeholder/placeholder.directiv
     selector: 'app-auth', 
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
   @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective;
 
+  private closeSubsiscription: Subscription;
+
   constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  ngOnDestroy() {
+    if (this.closeSubsiscription) {
+      this.closeSubsiscription.unsubscribe();
+    }
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -64,6 +72,12 @@ export class AuthComponent {
 
     const hostViewContainerRef = this.alertHost.viewContainerRef;
     hostViewContainerRef.clear();
-    hostViewContainerRef.createComponent(alertComponentFactory);
+    const componentRef = hostViewContainerRef.createComponent(alertComponentFactory);
+
+    componentRef.instance.message = errorMessage;
+    this.closeSubsiscription = componentRef.instance.close.subscribe(() => {
+      this.closeSubsiscription.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
