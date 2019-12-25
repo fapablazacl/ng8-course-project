08c96a30304b60f6a32b978b1192c576aca55868
Felipe Apablaza <ing.apablaza@gmail.com>
2019-12-25 19:55:41 -0300

    Added shopping list module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,14 +15,12 @@ import { AuthInterceptor } from './auth/auth-interceptor';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -37,7 +33,8 @@ import { RecipesModule } from './recipes/recipes.module';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [
     ShoppingListService, 
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { ShoppingListComponent } from "./shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ], 
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            { path: 'shopping-list', component: ShoppingListComponent  },
+        ])
+    ],
+    exports: [
+        ShoppingListComponent,
+        ShoppingEditComponent
+    ]
+})
+export class ShoppingListModule {
+
+}
