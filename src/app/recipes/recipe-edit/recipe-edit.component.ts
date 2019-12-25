b0edf9611f1c3cdc4570e49b0fd52deaca85a394
Felipe Apablaza <ing.apablaza@gmail.com>
2019-12-24 22:28:41 -0300

    Implemented cancel button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators, AbstractControl } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 
@@ -13,7 +13,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router: Router) { }
 
   ngOnInit() {
     this.route.params
@@ -63,7 +63,23 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.recipeForm);
+    /*
+    const formValue = this.recipeForm.value;
+    const recipe = new Recipe(
+      formValue.name,
+      formValue.description,
+      formValue.imagePath,
+      formValue.ingredients
+    );
+    */
+
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+
+    this.onCancel();
   }
 
   onAddIngredient(): void {
@@ -80,5 +96,9 @@ export class RecipeEditComponent implements OnInit {
 
   get controls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onCancel() {
+    this.router.navigate(["../"], {relativeTo: this.route});
   }
 }
